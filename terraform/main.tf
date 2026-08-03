511b55a72b41ae39d56278446afef685ed38ace7
Dawid <dbembnista1@gmail.com>
2026-08-03 21:43:10 +0200

    feat(tf): wire helm and kubernetes providers to EKS Authenticate Helm/Kubernetes providers via aws eks get-token so ArgoCD bootstrap can talk to the shared cluster.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35"
+    }
   }
 }
 
---
+++ terraform/providers-k8s.tf
@@ -0,0 +1,40 @@
+# Kubernetes and Helm talk to the shared EKS cluster (ArgoCD bootstrap and later addons).
+# Exec auth refreshes tokens during long applies; static aws_eks_cluster_auth tokens expire (~15m).
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      var.aws_region,
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        var.aws_region,
+      ]
+    }
+  }
+}
